ed8c4c4d23349d7caf5e1a5e49962a683f3f2ebe
tk <gwonhyeong.tak@gmail.com>
2019-01-27 22:45:33 +0900

    [title] add live555

---
+++ test/basic_env.cpp
@@ -0,0 +1,8 @@
+
+#include <gtest/gtest.h>
+#include "BasicUsageEnvironment.hh"
+#include "liveMedia.hh"
+
+TEST(live555, BasicUsageEnvironment) {
+    BasicTaskScheduler* ptr = BasicTaskScheduler::createNew();
+}
